d9ca371134d4300bbb2e63ae0034e1c4b1df784e
Vedanshs321 <vedanshs321@gmail.com>
2024-08-23 22:59:05 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,6 @@
+
+
+from pytube import Youtube
+link = "<Enter your youtube video link>" #example-https://www.youtube.com/watch?v=PbgUg_aBHf4
+download = Youtube.(link)
+download.strams.first().download()
